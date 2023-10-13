1a48f4b96d249782f54886e4dcdf9b31ecb01314
imjeongjin <kjj_010101@naver.com>
2023-10-13 11:04:11 +0900

    최종 DB 및 초기세팅 수정

--- db/migrations/20231012072403_createproductstable.sql
+++ db/migrations/20231012072403_createproductstable.sql
@@ -6,11 +6,10 @@ CREATE TABLE `products` (
   `price` INT NOT NULL,
   `quantity` INT NOT NULL,
   `discount_rate` INT,
-  `discount_price` INT,
   `product_category_id` INT NOT NULL,
   `seller_id` INT NOT NULL,
-  `created_at` TIMESTAMP NOT NULL,
-  `updated_at` TIMESTAMP,
+  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+  `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (product_category_id) REFERENCES product_categories (id) ON DELETE CASCADE,
   FOREIGN KEY (seller_id) REFERENCES sellers (id) ON DELETE CASCADE
 );
