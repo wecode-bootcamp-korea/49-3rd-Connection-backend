2d997b8a077960809e3c1618983fcfef71bf6f8d
신유진 <julia011097@gmail.com>
2023-10-17 03:17:50 +0900

    routers 폴더의 orderRouter.js 작성_이후 추가 예정

---
+++ src/routers/orderRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const orderController = require('../controllers');
+
+const router = express.Router();
+
+router.post('/', orderController.); // 넣어야 함 
+
+module.exports = { router }
