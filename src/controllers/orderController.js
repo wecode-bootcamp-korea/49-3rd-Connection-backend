dde85e34490e913ea1497d4925cf674c517dc662
신유진 <julia011097@gmail.com>
2023-10-17 03:45:19 +0900

    orderController.js 틀만 작성

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const orderService = require('../services');
 
-
 const B = async (req, res) => {
   try {
-    console.log('orderController connected')
+    console.log('orderController connected');
 
-    const token = req.headers.authorization; 
+    const token = req.headers.authorization;
     const { C } = req.body;
 
-  // service 파일의 비즈니스 로직으로 'content' 보냄
-  await postService. B ( C )
+    // service 파일의 비즈니스 로직으로 'content' 보냄
+    await postService.B(C);
 
+    const { id } = jwt.verify(token, process.env.TYPEORM_JWT);
 
-  const {id} = jwt.verify(token,process.env.TYPEORM_JWT);
+    console.log(id);
 
-  console.log(id);
-
-  return res.status(200).json({message: " B _ FAILED "}); 
-  } 
-  catch(error){
+    return res.status(200).json({ message: ' B _ FAILED ' });
+  } catch (error) {
     console.error('JWT verification failed:', err.message);
     console.log(error);
-    return res.status(400).json({message:"FAILED"});
+    return res.status(400).json({ message: 'FAILED' });
   }
+};
+
+module.exports = {
+  B,
 };
