488485731007ad5844e757334e203aaada14a395
신유진 <julia011097@gmail.com>
2023-10-18 15:01:36 +0900

    orderController.js_1)주문정보 저장_ 완료

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const orderService = require('../services');
+const { orderService } = require('../services');
 
 const createOrders = async (req, res) => {
   try {
     console.log('orderController connected');
+    const { totalPrice, shippingMethod, paymentId } = req.body;
 
+    await orderService.createOrders(totalPrice, shippingMethod, paymentId);
 
+    return res.status(200).json({ message: 'Created Orders 주문정보 저장' });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: 'FAILED creating orders 주문정보 저장 실패' });
+  }
+};
 
 module.exports = {
   createOrders,
