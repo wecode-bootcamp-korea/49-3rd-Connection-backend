8d7afd2f83b3fddf1ab9cf3b3aec2943fb6961e1
신유진 <julia011097@gmail.com>
2023-10-19 11:07:56 +0900

    장바구니 전체 삭제 및 선택 삭제 api _ orderService, orderDao 수정

--- src/models/orderDao.js
+++ src/models/orderDao.js
@@ -1,15 +1,13 @@
 const { AppDataSource } = require('./dataSource.js');
 
+// 1) orders table 주문 정보 저장
 const createOrders = async (
   userId,
+  orderId,
   totalPrice,
   shippingMethod,
-  paymentId,
-  orderId,
-  productId,
-  quantity
+  paymentId
 ) => {
-  // 1) orders table 주문 정보 저장
   const newOrder = await AppDataSource.query(`
     INSERT INTO 
       orders(
@@ -35,7 +33,8 @@ const createOrders = async (
 };
 
 // 2) orderDetails table 주문 정보 저장
-const neworderDetails = await AppDataSource.query(`
+const neworderDetails = async (orderId, productId, quantity) => {
+  await AppDataSource.query(`
     INSERT INTO 
       order_details(
         order_id,
@@ -47,13 +46,27 @@ const neworderDetails = await AppDataSource.query(`
       "${productId}", 
       "${quantity}" 
       ) 
-      `); // 함수명 회색이여도 괜찮음. 여기에서는 return을 해줄 필요 없음 -> 주문 정보 req에서 받아와서 저장이니, res에 보내줄 값이 없기에. (getPost일 땐 return 하겠지만)
+      `);
+}; // await 앞에 굳이 const로 함수명 정의해주지 않아도 됨. return할 때 필요한 건데, return 안하니
+// 함수명 회색이여도 괜찮음. 여기에서는 return을 해줄 필요 없음 -> 주문 정보 req에서 받아와서 저장이니, res에 보내줄 값이 없기에. (getPost일 땐 return 하겠지만)
 
-// 3) carts 에서 삭제
-const deletingCarts = await AppDataSource.query(`
+// 3) carts 에서 전체 삭제
+const deleteAllCarts = async (userId, productId, quantity) => {
+  await AppDataSource.query(`
     DELETE FROM carts WHERE user_id = ${userId} AND product_id = ${productId} AND quantity = ${quantity}
     `); // return 필요없음 (res 보내줄 값이 없음 )
+};
+
+// 4) carts 에서 부분 삭제
+const updateCarts = async (userId, productId, quantity) => {
+  await AppDataSource.query(`
+    UPDATE carts SET quantity= ${quantitiy}  WHERE user_id = ${userId} AND product_id = ${productId} 
+    `); // return 필요없음 (res 보내줄 값이 없음 )
+}; //장바구니 테이블 : 수량 변경만 하면 됨
 
 module.exports = {
   createOrders,
+  neworderDetails,
+  deleteAllCarts,
+  updateCarts,
 };
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -10,11 +10,6 @@ const createOrders = async (
   productId,
   quantity
 ) => {
-
-  if (!productId) {
-    throwError(400, 'no product');
-  }
-
   // 1) orders table 주문 정보 저장
   const newOrder = await orderDao.createOrders(
     userId,
@@ -23,30 +18,29 @@ const createOrders = async (
     paymentId
   ); // 주문정보 저장은 return해줄 필요 없음
 
-  const orderId = newOrder;      // orderDetails 에서 저장하기 위한 orderId 가져오기
+  const orderId = newOrder; // orderDetails 에서 저장하기 위한 orderId 가져오기
 
   // 2) orderDetails table 주문 정보 저장
-//   const newOrderDetails = await orderDao.createOrders(
-  await orderDao.createOrderDetails(
+  const newOrderDetails = await orderDao.createOrders(
+    // await orderDao.createOrderDetails(
     userId,
     orderId,
     productId,
     quantity
-  )
-
-
-  // 3) carts 에서 삭제
-  // carts 의 quantity 와 orderDetails의 quantity가 같으면(  if(cart에서 불러오는 Quantity == quantity)), 전체삭제 
-  // 같지 않다면(else), 부분 삭제 
-  if(cartQuantity == quantity)
-  else 
-  
-  const deletingCarts = await orderDao.deleteCartsProducts(
-    userId,
-    productId,
-    quantity
   );
 
+  // 3) carts 에서 삭제
+  // carts 의 quantity 와 orderDetails의 quantity가 같으면(  if(cart에서 불러오는 Quantity == quantity)), 전체삭제
+  // 같지 않다면(else), 부분 삭제
+  if (cartQuantity == quantity) {
+    const deleteAllCarts = await orderDao.deleteAllCarts(
+      userId,
+      productId,
+      quantity
+    );
+  } else {
+    const updateCarts = await orderDao.updateCarts(userId, productId, quantity);
+  }
 };
 
 module.exports = {
