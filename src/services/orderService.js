a00fa7ac2472cd4e19ca6dfbf178ec10df6ff38e
신유진 <julia011097@gmail.com>
2023-10-20 08:34:23 +0900

    postman_2차  통신 완료

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -19,8 +19,7 @@ const createOrders = async (
   const orderId = newOrder; // orderDetails 에서 저장하기 위한 orderId 가져오기
   console.log(orderId);
 
-
-  if( )    // orderDetails 로 들어가는 게 실패했는데, order 테이블에 계속 추가되는 경우 
+  //  if( )    // orderDetails 로 들어가는 게 실패했는데, order 테이블에 계속 추가되는 경우
   // 2) orderDetails table 주문 정보 저장
   const newOrderDetails = await orderDao.newOrderDetails(
     orderId,
@@ -31,7 +30,7 @@ const createOrders = async (
   const cartQuantity = await orderDao.cartQuantity(userId, productId); // await: userId, productId,(orderDetails의 ) quantity를  orderDao로 보내준다
   console.log(cartQuantity);
 
-  if () // 장바구니에 있는 cart < 주문 들어온 수량 클 때  (quantity 들어오고 나서, 에러 내보내야 함 )
+  //  if () // 장바구니에 있는 cart < 주문 들어온 수량 클 때  (quantity 들어오고 나서, 에러 내보내야 함 )
 
   const updateQuantity = cartQuantity - quantity;
   // 3) carts 에서 삭제:  carts 의 quantity 와 orderDetails의 quantity가 같으면( if(cart에서 불러오는 Quantity == quantity)): 전체삭제, 같지 않다면(else): 부분 삭제
