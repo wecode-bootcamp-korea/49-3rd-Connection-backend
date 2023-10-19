33b4574cabf7ba4e6fd84d981983f1dd033e9942
RYU <kshykl9@gamil.com>
2023-10-19 12:50:44 +0900

    productList 수정

--- tests/product.test.js
+++ tests/product.test.js
@@ -48,13 +48,13 @@ describe('get product', () => {
   });
 
   test('SUCCESS: get list by sellerId 1', async () => {
-    const res = await request(app).get('/products/seller/1');
+    const res = await request(app).get('/products?sellerId=1');
     expect(res.status).toBe(200);
     expect(res.body.message).toEqual('Success');
   });
 
   test('SUCCESS: get list by categoryId 1', async () => {
-    const res = await request(app).get('/products/?categoryId=1');
+    const res = await request(app).get('/products?categoryId=1');
     expect(res.status).toBe(200);
     expect(res.body.message).toEqual('Success');
   });
