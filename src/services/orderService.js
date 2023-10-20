a82436a2f9feee2ff797fa3887ecf8d2cb10f308
신유진 <julia011097@gmail.com>
2023-10-20 13:51:20 +0900

    브랜치 이동

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -6,7 +6,8 @@ const createOrders = async (
   shippingMethod,
   paymentId,
   productId,
-  quantity
+  quantity,
+
 ) => {
   // 1) orders table 주문 정보 저장
   const newOrder = await orderDao.createOrders(
@@ -19,7 +20,9 @@ const createOrders = async (
   const orderId = newOrder; // orderDetails 에서 저장하기 위한 orderId 가져오기
   console.log(orderId);
 
-  //  if( )    // orderDetails 로 들어가는 게 실패했는데, order 테이블에 계속 추가되는 경우
+  if( ) // user 포인트가 없을 때 
+  if( ) //cart 에 없는 걸 주문할 뗴 -> productId 
+  if( )    // orderDetails 로 들어가는 게 실패했는데, order 테이블에 계속 추가되는 경우
   // 2) orderDetails table 주문 정보 저장
   const newOrderDetails = await orderDao.newOrderDetails(
     orderId,
@@ -30,7 +33,7 @@ const createOrders = async (
   const cartQuantity = await orderDao.cartQuantity(userId, productId); // await: userId, productId,(orderDetails의 ) quantity를  orderDao로 보내준다
   console.log(cartQuantity);
 
-  //  if () // 장바구니에 있는 cart < 주문 들어온 수량 클 때  (quantity 들어오고 나서, 에러 내보내야 함 )
+  if () // cart 수량 < 주문 수량 (quantity니까 orderDetails) (quantity 들어오고 나서, 에러 내보내야 함 )
 
   const updateQuantity = cartQuantity - quantity;
   // 3) carts 에서 삭제:  carts 의 quantity 와 orderDetails의 quantity가 같으면( if(cart에서 불러오는 Quantity == quantity)): 전체삭제, 같지 않다면(else): 부분 삭제
