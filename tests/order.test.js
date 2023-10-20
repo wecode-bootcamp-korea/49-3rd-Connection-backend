3a49122744c103aa72028680985f64589b84f7b0
신유진 <julia011097@gmail.com>
2023-10-20 22:07:50 +0900

    test code

--- tests/order.test.js
+++ tests/order.test.js
@@ -16,112 +16,54 @@ describe('make order and pay', () => {
 
     // 순서가 중요함 -> data 들어가는 flow 에 맞춰서
     // 하드 코딩해서 직접 값을 key에 입력해주는 것
+    // users 테이블 채우기
     await AppDataSource.query(`
-      SELECT points FROM users WHERE id = 1
+    INSERT INTO users (name, email, password, phone_number, zip_code, address, address_details) VALUES ('ddd', '@', '111', '010', '00000', '강남구', '101동')  
     `);
+    // sellers 채우기
     await AppDataSource.query(`
-      SELECT product_id FROM carts WHERE user_id = 1 AND product_id = 1 
+    INSERT INTO sellers (name, image, zip_code, address, address_details, phone_number) VALUES ('ddd', 'url', '111', '강남구', '101동 ', '010')  
     `);
+    // products 테이블 채우기
     await AppDataSource.query(`
-    INSERT INTO orders ( user_id, total_price, shipping_method, payment_id)
-    VALUES (1, '2000', '택배 배송', 1);
+    INSERT INTO product_categories (category_name) VALUES ('뷰티') 
+  `);
+    // products 테이블 채우기
+    await AppDataSource.query(`
+    INSERT INTO products (name, images, price, product_category_id, seller_id) VALUES ('감자',"url",'1000', 1, 1) 
     `);
+    // carts 채우기
     await AppDataSource.query(`
-    INSERT INTO order_details ( order_id, product_id, quantity) 
-    VALUES ( 1, 1, 10);
+    INSERT INTO carts (user_id, product_id, quantity) VALUES (1,1,3) 
     `);
+    // paymemts 채우기
     await AppDataSource.query(`
-    UPDATE users SET points = '2000' WHERE id = 1 ;
-    `);
-    await AppDataSource.query(`
-    DELETE FROM users WHERE user_id = 1 AND product_id = 1 ;
-    `);
-    await AppDataSource.query(`
-    UPDATE carts SET quantity = 30 WHERE user_id = 1 AND product_id = 1 ; 
+    INSERT INTO payments (method) VALUES ("s") 
     `);
   });
 
   afterAll(async () => {
-    // 테스트 데이터베이스의 불필요한 데이터를 전부 지워줍니다. -> 그래서 .env 에 test database 넣어야 함
-    await AppDataSource.query(`SET foreign_key_checks = 0;`);
-    await AppDataSource.query(`TRUNCATE reviews`);
+    // truncate: 특정 테이블 비움; 테스트 데이터베이스의 불필요한 데이터를 전부 지워줍니다. -> 그래서 .env 에 test database 넣어야 함
+    // 위에 table들 여기에 넣음 _ 근데 flow 순서대로 userId > productId > carts> orderId 등
+    await AppDataSource.query(`SET foreign_key_checks = 0;`); //외래키 비활성화
+    await AppDataSource.query(`TRUNCATE payments`);
+    await AppDataSource.query(`TRUNCATE carts`);
     await AppDataSource.query(`TRUNCATE products`);
     await AppDataSource.query(`TRUNCATE product_categories`);
-    await AppDataSource.query(`TRUNCATE users`);
     await AppDataSource.query(`TRUNCATE sellers`);
+    await AppDataSource.query(`TRUNCATE users`);
+    //   await AppDataSource.query(`SET foreign_key_checks = 1;`); // 외래키 다시 활성화
 
     // 모든 테스트가 끝나게 되면(afterAll) DB 커넥션을 끊어줍니다.
     await AppDataSource.destroy();
   });
 
   test('SUCCESS: created orders ', async () => {
-    const res = await request(app).post('/orders');
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual({
-      message: 'message": "Created Orders 주문정보 저장" ',
-      data: [
-        {   
-          userId: 1,
-          totalPrice: "1000" , 
-          shippingMethod : "방문수령",
-          paymentId : 1,
-          productId : 1,
-          quantity : 1
-      }
-      ],
-    });
+    await request(app)
+      .post('/orders')
+      .send({ userId: 1, totalPrice: '1000', shippingMethod: '방문수령', paymentId: 1, productId: 1, quantity: 1 });
+      .expect(201);
+//      expect(res.status).toBe(200);
+//    expect(res.body.message).toEqual('Created Orders 주문정보 저장');
   });
-
-  test('SUCCESS: created ordersDetails ', async () => {
-    const res = await request(app).post('/orders');
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual({
-      message: 'message": "Created OrderDetails 결제 후 주문내역 저장" ',
-      data: [
-        {   
-          userId: 1,
-          totalPrice: "1000" , 
-          shippingMethod : "방문수령",
-          paymentId : 1,
-          productId : 1,
-          quantity : 1
-      }
-      ],
-    });
-  });
-
-  test('SUCCESS: created orders ', async () => {
-    const res = await request(app).get('/products/category');
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual({
-      message: 'message": "Created Orders 주문정보 저장" ',
-      data: [
-        {   
-          userId: 1,
-          totalPrice: "1000" , 
-          shippingMethod : "방문수령",
-          paymentId : 1,
-          productId : 1,
-          quantity : 1
-      }
-      ],
-    });
-  });
-
-  test('SUCCESS: created orders ', async () => {
-    const res = await request(app).get('/products/category');
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual({
-      message: 'message": "Created Orders 주문정보 저장" ',
-      data: [
-        {   
-          userId: 1,
-          totalPrice: "1000" , 
-          shippingMethod : "방문수령",
-          paymentId : 1,
-          productId : 1,
-          quantity : 1
-      }
-      ],
-    });
-  });
+}, 10000000000);
