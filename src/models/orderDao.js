f2e2410a7afc4950af3c245a75baba8b3b2c4fe9
신유진 <julia011097@gmail.com>
2023-10-20 15:26:56 +0900

    에러 핸들링, 결제 (포인트 차감) 추가

--- src/models/orderDao.js
+++ src/models/orderDao.js
@@ -1,5 +1,26 @@
 const { AppDataSource } = require('./dataSource');
 
+// 에러 핸들링: user 포인트가 부족할 때 ( 갖고 있는 point보다 비싼 걸 살 때)
+const isUsersPoints = async (userId) => {
+  const usersPoints = await AppDataSource.query(`
+  SELECT points FROM users WHERE user_id = ${userId} 
+  `);
+  console.log(
+    '주문 전, 유저 points 부족하진 않은지 확인',
+    usersPoints[0].points
+  );
+  return usersPoints[0].points;
+};
+
+// 에러 핸들링 : carts 에 없는 product id 주문 시, 에러 핸들링 위한 carts의 product id 가져오기
+const isProductInCarts = async (userId, productId) => {
+  const cartsProductId = await AppDataSource.query(`
+  SELECT productId FROM carts WHERE user_id = ${userId} AND product_id = ${productId}
+  `);
+  console.log('장바구니에 있는 product 인지 확인', cartsProductId[0].productId);
+  return cartsProductId[0].productId;
+};
+
 // 1) orders table 주문 정보 저장
 const createOrders = async (userId, totalPrice, shippingMethod, paymentId) => {
   const newOrder = await AppDataSource.query(`
@@ -17,7 +38,7 @@ const createOrders = async (userId, totalPrice, shippingMethod, paymentId) => {
       '${paymentId}'
     ) 
     `);
-  console.log('방금 수행한 작업 정보:', newOrder);
+  console.log('1) order 테이블에 주문 정보 저장:', newOrder);
 
   console.log('orderId:', newOrder.insertId);
   return newOrder.insertId;
@@ -41,7 +62,31 @@ const newOrderDetails = async (orderId, productId, quantity) => {
 }; // await 앞에 굳이 const로 함수명 정의해주지 않아도 됨. return할 때 필요한 건데, return 안하니
 // 함수명 회색이여도 괜찮음. 여기에서는 return을 해줄 필요 없음 -> 주문 정보 req에서 받아와서 저장이니, res에 보내줄 값이 없기에. (getPost일 땐 return 하겠지만)
 
-// 장바구니 삭제를 위한 수량 가져오기
+// 3) 결제: 포인트 차감을 위한 user points 가져오기
+const userPoints = async (userId) => {
+  const pointsFromUser = await AppDataSource.query(`
+  SELECT points FROM users WHERE user_id = ${userId} 
+  `);
+  console.log('결제 직전 위한 유저 points 가져오기', pointsFromUser[0].points);
+  return pointsFromUser[0].points;
+}; // quantity는 'select 문을 위해' 받아올 필요 없음 , select문으로 구할 값!
+// console.log('카트에 들어있는 수량 :', cartQuantity);
+
+// 3)-1 points 전체 차감
+const deleteAllPoints = async (userId, userPoints) => {
+  await AppDataSource.query(`
+    DELETE FROM users.points WHERE user_id = ${userId} AND user_points = ${userPoints}
+    `); // return 필요없음 (res 보내줄 값이 없음 )
+};
+
+// 3)-2 points 부분 차감
+const updatePoints = async (userId, updatePoints) => {
+  await AppDataSource.query(`
+    UPDATE users SET  points = '${updatePoints}'  WHERE user_id = ${userId} 
+    `); // return 필요없음 (res 보내줄 값이 없음 )
+}; //장바구니 테이블 : 수량 변경만 하면 됨
+
+// 4) 장바구니 삭제를 위한 수량 가져오기
 const cartQuantity = async (userId, productId) => {
   const result = await AppDataSource.query(`
   SELECT quantity FROM carts WHERE user_id = ${userId} AND product_id = ${productId}
@@ -51,14 +96,14 @@ const cartQuantity = async (userId, productId) => {
 }; // quantity는 'select 문을 위해' 받아올 필요 없음 , select문으로 구할 값!
 // console.log('카트에 들어있는 수량 :', cartQuantity);
 
-// 3) carts 에서 전체 삭제
+// 4)-1 carts 에서 전체 삭제
 const deleteAllCarts = async (userId, productId) => {
   await AppDataSource.query(`
     DELETE FROM carts WHERE user_id = ${userId} AND product_id = ${productId} 
     `); // return 필요없음 (res 보내줄 값이 없음 )
 };
 
-// 4) carts 에서 부분 삭제
+// 4)-2 carts 에서 부분 삭제
 const updateCarts = async (userId, productId, updateQuantity) => {
   await AppDataSource.query(`
     UPDATE carts SET  quantity = '${updateQuantity}'  WHERE user_id = ${userId} AND product_id = ${productId} 
@@ -66,8 +111,13 @@ const updateCarts = async (userId, productId, updateQuantity) => {
 }; //장바구니 테이블 : 수량 변경만 하면 됨
 
 module.exports = {
+  isUsersPoints,
+  isProductInCarts,
   createOrders,
   newOrderDetails,
+  userPoints,
+  deleteAllPoints,
+  updatePoints,
   cartQuantity,
   deleteAllCarts,
   updateCarts,
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -20,9 +20,20 @@ const createOrders = async (
   const orderId = newOrder; // orderDetails 에서 저장하기 위한 orderId 가져오기
   console.log(orderId);
 
-  if( ) // user 포인트가 없을 때 
-  if( ) //cart 에 없는 걸 주문할 뗴 -> productId 
-  if( )    // orderDetails 로 들어가는 게 실패했는데, order 테이블에 계속 추가되는 경우
+  // 에러 핸들링: order.totalPrice > user : 포인트가 부족할 때  ( 갖고 있는 point보다 비싼 걸 살 때) 
+  if( totalPrice > isUsersPoints ){
+    throw new Error('user 포인트 부족; 가진 포인트보다 비싼 거 사려고 함')
+  } 
+  
+  // 에러 핸들링: cart 에 없는 걸 주문할 뗴 -> productId 
+  if( productId !== isProductInCarts){
+    throw new Error('장바구니에 없는 product id를 주문함')
+  } 
+  // 에러 핸들링  transaction 1 : orderDetails 로 insert into 실패했는데, order 테이블에 계속 추가되는 경우
+  if(  ){
+    throw new Error('2단계 orderDetails 주문정보 저장 실패, 처음으로 롤백! ')
+  } 
+
   // 2) orderDetails table 주문 정보 저장
   const newOrderDetails = await orderDao.newOrderDetails(
     orderId,
@@ -33,10 +44,37 @@ const createOrders = async (
   const cartQuantity = await orderDao.cartQuantity(userId, productId); // await: userId, productId,(orderDetails의 ) quantity를  orderDao로 보내준다
   console.log(cartQuantity);
 
-  if () // cart 수량 < 주문 수량 (quantity니까 orderDetails) (quantity 들어오고 나서, 에러 내보내야 함 )
+  // 에러 핸들링 : cart에 담은 수량  < 주문한 수량 (quantity니까 orderDetails에 주문 저장 후, 에러 내보내야 함 )
+  if( cartQuantity < quantity  ){
+    throw new Error('장바구니에 담은 수량보다, 더 많이 주문함 ')
+  } 
 
+
+  // 에러 핸들링  transaction 2 : 결제 단계로 실패했는데, (결제 후 단계) orderdetails 테이블에 계속 추가되는 경우
+  if(  ){
+    throw new Error('3단계 결제 실패, 처음으로 롤백! ')
+  } 
+
+  // 3) 결제 users 의 points 부분 차감 : users 의 points 와 orderDetails의 totalPrice 가 다른 경우에, UPDATE 수정을 위한 계산식 
+  const updatePoints = userPoints - totalPrice
+  //3) 결제 : users 의 points 와 orderDetails의 totalPrice 가 같으면: 전체삭제, 같지 않다면(else): 부분 삭제
+    if (userPoints == totalPrice) {
+      const deleteAllPoints = await orderDao.deleteAllCarts(
+        userId,
+        productId,
+        quantity
+      ); // 위에서 받아온 cartQuantity, orderdetails에서 받은 quantity
+    } else {
+      const updatedPoints = await orderDao.updateCarts(
+        userId,
+        productId,
+        updateQuantity
+      );
+    } 
+  
+  // 4) carts 에서 부분삭제:  carts 의 quantity 와 orderDetails의 quantity가 다른 경우에, UPDATE 수정 위한 계산식
   const updateQuantity = cartQuantity - quantity;
-  // 3) carts 에서 삭제:  carts 의 quantity 와 orderDetails의 quantity가 같으면( if(cart에서 불러오는 Quantity == quantity)): 전체삭제, 같지 않다면(else): 부분 삭제
+  // 4) carts 에서 삭제:  carts 의 quantity 와 orderDetails의 quantity가 같으면( if(cart에서 불러오는 Quantity == quantity)): 전체삭제, 같지 않다면(else): 부분 삭제
   if (cartQuantity == quantity) {
     const deleteAllCarts = await orderDao.deleteAllCarts(
       userId,
