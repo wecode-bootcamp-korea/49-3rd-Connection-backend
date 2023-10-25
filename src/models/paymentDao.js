8ec14e55decd8ff554cf17e8d8ed650228f6b9e8
Jung Jihyun <wlgussl0215@gmail.com>
2023-10-25 22:32:19 +0900

    UPDATE

--- src/models/paymentDao.js
+++ src/models/paymentDao.js
@@ -28,15 +28,13 @@ const createPayment = async (method, totalPrice, shippingMethod, userId) => {
       );
       console.log('ordersresult', ordersresult);
 
-      // 트랜잭션 내에서 모든 쿼리가 성공적으로 실행되었을 때 "result" 변수에 어떤 값을 할당할 수도 있습니다.
       return ordersresult;
     } catch (error) {
-      // 에러 처리 로직
-      throw error; // 에러를 상위로 다시 던지거나 적절하게 처리
+      throw error;
     }
   });
   console.log('result', result);
-  return result; // 여기서도 "result"를 반환
+  return result;
 };
 
 module.exports = {
